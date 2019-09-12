ade82d2abe2cdb80e4a1e8e09cf4e98495beed2d
ahmadnazif <ahmadnazif@gmail.com>
2019-09-12 18:23:26 +0800

    Some changes on main class

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
-import requests as api
-import models.SimpleHttpResponse as httpResp
+import json
 import models.MtRequest as _mtReq
-import models.MtResponse as mtResp
 import core.WebApiSender as _api
 
 baseUrl = "http://162.253.16.28/api/send"
-
-mtReq = _mtReq.MtRequest(
-    "httppostpaid", #username
-    "123456", #password
-    "Python", #From
-    "60123456789", #To
-    "Python sample" #Text
-    )
-
 api = _api.WebApiSender
 
+# TODO: change according to your own data
+# for username & password. If you set 'dlrMask' to 1,
+# please specify the 'dlrUrl'
+
+mtReq = _mtReq.MtRequest(
+    "httppostpaid",  # username
+    "123456",  # password
+    "Python",  # from
+    "60123456789",  # to
+    "Python sample"  # Text
+)
+
+# METHODS ----------------------------------------
 def sendGet():
     print("Executing GET request..")
-    resp = api.sendGetRequest(baseUrl)
+
+    url = baseUrl+"?gw-username="+mtReq.username+"&gw-password="+mtReq.password+"&gw-from="+mtReq.from_+"&gw-to="+mtReq.to+"&gw-text="+mtReq.text
+
+    resp = api.sendGetRequest(url)    
+    print(resp.responseContentString)
+
 
 def sendPost():
     print("Executing POST request")
 
+
 def test(data):
     print(data)
 
+
+# CALLER -----------------------------------------
+# TODO: change this between 1 - 2 to switch result
+# 1 = Send using POST
+# 2 = Send using GET
+
+type = 2
+if type == 1:
+    sendPost()
+elif type == 2:
+    sendGet()
--- core/WebApiSender.py
+++ core/WebApiSender.py
@@ -1,11 +1,16 @@
 import requests as api
 import models.SimpleHttpResponse as httpResp
+
 class WebApiSender: 
 
-    def sendGetRequest(baseUrl):
-        resp = api.get(baseUrl)
+    def sendGetRequest(url):
+        resp = api.get(url)
         return httpResp.SimpleHttpResponse(
             resp.status_code,
             resp.text,
             '')
+
+    def sendPostRequest(baseUrl, jsonBody):
+        resp = api.post(url=baseUrl, json=jsonBody)
+        
         
